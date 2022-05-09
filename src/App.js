f36f4c41d6660b4d1a9aba3630e068689fb18787
alumno <alumno@vm1>
2022-05-09 15:25:29 +0200

    Login y registro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Redirect, Route } from 'wouter';
+import Home from './Paginas/Home/Home';
+import Registro from './Paginas/Auth/Registro';
+import Login from './Paginas/Auth/Login';
+import axios from 'axios';
+
+axios.defaults.headers.post['Content-type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contenido'>
+      <Route
+        component={Home}
+        path="/">
+      </Route>
+      <Route
+        path="/Login">
+        {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
+      </Route>
+      <Route
+        path="/register">
+        {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Registro />}
+      </Route>
     </div>
   );
 }
---
+++ src/Paginas/Home/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "../../Layouts/Navbar";
+
+function Home() {
+    
+    return(
+        <div>
+            <Navbar/>
+            <h1>Home</h1> 
+        </div>
+        
+    );
+}
+
+export default Home;
